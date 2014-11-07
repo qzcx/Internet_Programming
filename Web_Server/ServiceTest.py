9709e5ef8b88aac087aced4e1a0b8511d9c70fb6
Jon George <jgeorge@byu.edu>
2014-11-07 12:32:08 -0700

    service tests

---
+++ Web_Server/ServiceTest.py
@@ -0,0 +1,25 @@
+import requests
+import os
+import time
+
+def GetServiceSpeed(url, outputFile, results):
+    f = open(outputFile,'a')
+    start = time.time()
+    requests.get(url, stream=True)
+    end = time.time()
+    dif = end - start
+
+    #f.write(str(dif) + "\n" )
+    results.append(dif)
+
+def RunNTimes(n):
+    GetServiceSpeed("http://localhost:3000/file005.txt", "./ServiceTest.txt", [])
+    for i in range(0,n):
+        results = []
+        GetServiceSpeed("http://localhost:3000/file005.txt", "./ServiceTest.txt", results)
+    ans = sum(results)/float(len(results))
+    print ans
+    print 1/ans
+
+if __name__ == '__main__':
+    RunNTimes(100)
